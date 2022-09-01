3a9482e72221944689194aee8671a9fa2279b21f
saurabhkmahajan <saurabhmahajan605@gmail.com>
2022-09-01 23:54:10 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import requests
+
+
+# Create your views here.
+
+def home(request):
+
+    city = request.GET.get('city', 'Pune')
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=adf1aa920646c65752fe0f4098f487f1'
+    data = requests.get(url).json()
+
+    payload = {
+        'city': data['name'],
+        'weather': data['weather'][0]['main'],
+        'icon': data['weather'][0]['icon'],
+        'kelvin_temperature': data['main']['temp'],
+        'celcius_temperature': int(data['main']['temp'] -273),
+        'pressure': data['main']['pressure'],
+        'humidity': data['main']['humidity'],
+        'description' : data['weather'][0]['description']
+        }
+
+    context = {'data': payload}
+    print(context)
+    return render(request, 'home.html', context)
